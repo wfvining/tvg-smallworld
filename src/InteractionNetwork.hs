76aa980a68f243c69e3b7b18c49ce8b2bd21bdce
Will Vining <wfvining@gmail.com>
2018-04-11 16:39:05 -0600

    Fixed definition of `adjacencyMatrix` and `interactionNetwork`
    Forgot to provide matrix dimensions

--- src/InteractionNetwork.hs
+++ src/InteractionNetwork.hs
@@ -1,11 +1,16 @@
 module InteractionNetwork
   ( adjacencyMatrix
+  , interactionNetwork
   ) where
 
+import Data.Sparse.SpMatrix
 import Numeric.LinearAlgebra.Sparse
 
 type Snapshot = SpMatrix Double
 type Network = [Snapshot]
 
-adjacencyMatrix :: [(Int,Int)] -> Snapshot
-adjacencyMatrix = fromListSM . map (\(x, y) -> (x,y,1))
+adjacencyMatrix :: Int -> [(Int,Int)] -> Snapshot
+adjacencyMatrix numNodes interactions = fromListSM (numNodes,numNodes) $ map (\(x, y) -> (x,y,1)) interactions
+
+interactionNetwork :: Int -> [[(Int, Int)]] -> Network
+interactionNetwork size = map (adjacencyMatrix size)
